2cf9b73a3e1bfd7137b285593589e814eff68482
Marcello Barile <marcello.barile@gmail.com>
2018-04-15 00:13:55 +0200

    fixed unit test

--- test/unit/spec.js
+++ test/unit/spec.js
@@ -2,8 +2,8 @@ var test = require('tap').test;
 var sentiment = require('../../lib/index');
 
 test('interface', function (t) {
-    t.type(sentiment, 'en', 'function', 'module is a function');
-    t.type(sentiment('test'), 'object');
-    t.type(sentiment('test', {test: 10}), 'object');
+    t.type(sentiment, 'function', 'module is a function');
+    t.type(sentiment('test', 'en'), 'object');
+    t.type(sentiment('test', 'en', {test: 10}), 'object');
     t.end();
 });
